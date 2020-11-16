726514f8ad4567a0311a4ce54791d965be01478b
Zain Padela <zpadela@gmail.com>
2020-11-16 15:22:31 -0500

    Created skeleton for react components

---
+++ client/src/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import carousel from './carousel.jsx'
+import modal from './modal.jsx'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return()
+  }
+}
+
+export default App;
